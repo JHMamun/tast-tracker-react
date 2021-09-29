6fbfcbedc64bc43d38edf6466cd53d2c1bb01e3e
JHMamun <jahidhasanmamun1994@gmail.com>
2021-09-30 04:02:23 +0600

    done with button

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Tasks
